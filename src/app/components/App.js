e7303c30182db4b7947e217435910213c2a94c92
henryhkyim <40025988+henryhkyim@users.noreply.github.com>
2018-09-01 23:45:55 +0800

    Add stock quote code

--- src/app/components/App.js
+++ src/app/components/App.js
@@ -4,12 +4,14 @@ import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 import { Header } from './Header'
 import { Home } from './Home'
 import { FlashGame } from './flashgame/FlashGame'
-import { StockQuote } from './StockQuote'
+import { StockQuote } from './stockquote/StockQuote'
 import { Portfolio } from './Portfolio'
 import '../css/App.css'
 
 export class App extends React.Component {
 	render() {
+		let stockList = ["00005.HK", "00006.HK", "00700.HK", "07336.HK", "00005.HK", "00006.HK", "00700.HK", "07336.HK",
+									   "00005.HK", "00006.HK"]
 		return (
 				<BrowserRouter>
 					<div>
@@ -21,7 +23,7 @@ export class App extends React.Component {
 							  <Redirect exact from="/" to="/home"/>
 								<Route path="/home" exact component={Home}/>
 								<Route path="/flashGame" exact component={FlashGame}/>
-								<Route path="/stockQuote" exact component={StockQuote}/>
+								<Route path="/stockQuote" exact component={() => <StockQuote stockList={stockList}/>}/>
 								<Route path="/portfolio" exact component={Portfolio}/>
 							</Switch>
 						</div>
---
+++ src/app/components/stockquote/StockQuote.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import { OneStockQuote } from './OneStockQuote.js'
+
+import '../../css/stockquote/StockQuote.css'
+
+export class StockQuote extends React.Component {
+
+	renderQuote(symbol, index) {
+		return <OneStockQuote key={index} stock={symbol}/>
+	}
+
+	render() {
+		return (
+			<div>
+				<h2>Stock Quote</h2>
+				<p>This is a reactjs sample demostrating the real time quote data refreshing.</p>
+				<p className="redbold">*** This is just a sample page, the price are hypothetical and random generated. ***</p>
+				<hr/>
+				<span className="alignCenter">
+					{this.props.stockList.map(this.renderQuote)}
+				</span>
+				<div className="clearFloat"></div>
+			</div>
+		)
+	}
+}
+
+StockQuote.propTypes = {
+	stockList: PropTypes.array
+}
